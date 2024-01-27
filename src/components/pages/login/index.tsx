b60c1cdffed73d03e4a7526d207b1207c628cdad
obirijatochukwu <tobirijah@gmail.com>
2024-01-27 13:14:17 -0800

    kc

--- src/components/pages/login/index.tsx
+++ src/components/pages/login/index.tsx
@@ -31,19 +31,22 @@ export default function Login() {
   }, [form?.email]);
 
   return (
-    <main>
+    <main
+      style={{ backgroundColor: "#2148C0" }}
+      className="flex justify-center bg-[url('/media/login/BG.svg')] bg-opacity-30 items-center h-screen bg-cover bg-no-repeat bg-center w-screen overflow-y-auto"
+    >
       <form
         onSubmit={login}
         className="flex flex-col w-80 items-center justify-center gap-4"
       >
-        <div className="text-xl font-bold text-primary">Login</div>
-        <div className="border-b w-14 border-secondary"></div>
-        <div className="text-base text-opacity-70 text-primary text-center leading-5">
+        <div className="text-xl font-bold text-white">Login</div>
+        <div className="border-b w-14 border-white"></div>
+        <div className="text-base text-opacity-70 text-white text-center leading-5">
           Welcome back! Login to create awesome Chatbots within minutes!
         </div>
         {inputs.map(({ name, placeholder }) => (
           <div className="w-full">
-            <div className=" text-base text-primary text-opacity-70 capitalize">
+            <div className=" text-base text-white text-opacity-70 capitalize">
               {name}*
             </div>
             <input
@@ -51,11 +54,12 @@ export default function Login() {
               required
               // autuFocus
               placeholder={placeholder}
+              //@ts-ignore
               value={form[name]}
               onChange={(e: any) =>
                 setForm({ ...form, [name]: e.target.value })
               }
-              className="h-12 border-2 w-full border-primary focus:outline-secondary border-opacity-70 rounded flex items-center px-4 text-lg text-primary"
+              className="h-12 border-2 w-full border-white focus:outline-secondary border-opacity-70 rounded flex items-center px-4 text-lg text-white"
             />
           </div>
         ))}
@@ -64,20 +68,23 @@ export default function Login() {
           <Buttons.primary
             title="Login"
             type="submit"
-            classname="!px-5 !h-10 rounded hover:shadow-2xl !text-lg"
+            classname="!px-5 !font-semibold !h-10 rounded hover:shadow-2xl !bg-white text-secondary !text-lg"
           />
         </div>
         <div>
-          <div className="text-base cursor-pointer text-center font-medium text-secondary">
+          <div className="text-base cursor-pointer text-center font-medium text-white">
             Forget Password
           </div>
-          <div className="text-base cursor-pointer text-center font-medium text-secondary">
+          <div className="text-base cursor-pointer text-center font-medium text-white">
             Verify Account
           </div>
         </div>
-        <div className="flex items-center justify-center capitalize">
-          new to chatbot?{" "}
-          <a className="text-base font-medium text-secondary">Signup</a>
+        <div className="flex gap-0.5 text-opacity-60 items-center text-white justify-center capitalize">
+          new to chatbot?
+          <a className="text-base font-medium text-white cursor-pointer">
+            {" "}
+            Signup
+          </a>
         </div>
       </form>
     </main>
